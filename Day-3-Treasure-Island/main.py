9f006c530d8a14b0873d47093e14dd03d258abaa
Urvi Vipani <urvi.vipani@gmail.com>
2024-03-09 18:18:09 -0600

    Create main.py

---
+++ Day-3-Treasure-Island/main.py
@@ -0,0 +1,48 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#Write your code below this line 👇
+
+first_choice = input("Where do you want to go left or right?: ")
+first_choice = first_choice.lower()
+if first_choice == "left":
+  second_choice = input("What will you do, swim or wait?: ")
+  second_choice = second_choice.lower()
+  if second_choice == 'wait':
+    third_choice = input("Which door would you like to choose? red, blue or yellow?: ")
+    third_choice = third_choice.lower()
+    if third_choice == 'red':
+      print('Burned by fire. Game Over')
+    elif third_choice == 'blue':
+      print('Eaten by beasts. Game Over')
+    elif third_choice == 'yellow':
+      print('You Win!')
+    else:
+      print('Game over')
+      
+  else:
+    print('Attacked by trout. Game Over.')
+else:
+  print("Fall into a hole. Game Over")
